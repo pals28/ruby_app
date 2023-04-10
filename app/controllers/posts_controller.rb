b2c004b934465d3a9845d197c214a4686b093dca
Palvi Shah <palvi.shah@breakingwave.io>
2023-04-10 11:14:42 +0100

    add post model and param check

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,26 @@ class PostsController < ApplicationController
 
     def index
     end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        # Controller instance variables such as @post, can be accessed by the view
+        @post = Post.new(check_params)
+        if @post.save 
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    private
+
+    def check_params
+        # Creates an instance of ActionController::Parameters with the key passed in require
+        params.require(:post).permit(:title, :content)
+    end
+
 end
