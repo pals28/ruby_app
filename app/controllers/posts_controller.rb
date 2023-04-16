b2a76b184aa482faf107c8d3ae4b105405fb80ed
Palvi Shah <palvi.shah@breakingwave.io>
2023-04-16 09:51:29 +0100

    added edit functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,7 @@ class PostsController < ApplicationController
 
     def create
         # Controller instance variables such as @post, can be accessed by the view
-        @post = Post.new(check_params)
+        @post = Post.new(post_params)
         if @post.save 
             redirect_to @post
         else
@@ -22,9 +22,30 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+
+    def edit 
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        
+        redirect_to root_path
+    end
+
     private
 
-    def check_params
+    def post_params
         # Creates an instance of ActionController::Parameters with the key passed in require
         params.require(:post).permit(:title, :content)
     end
